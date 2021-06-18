6899a9444e4e7caa22506cae4f25366c8586bc4f
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-06-18 13:16:58 -0700

    ...

--- diffdts/packages/diffdts/src/index.ts
+++ diffdts/packages/diffdts/src/index.ts
@@ -1,9 +1,27 @@
 import * as path from "path";
 import * as fs from "fs";
 
-const dirbase = path.join(__dirname, "../../../../download/node_modules/");
-const dirsrc = path.join(dirbase, "react-native-tscodegen-types/react-native/index.d.ts");
-const dirdst = path.join(dirbase, "@types/react-native/index.d.ts");
+function processEntry(inputFilename: string, outputFilename: string): void {
+  fs.writeFileSync(
+    outputFilename,
+    JSON.stringify(
+      { filename: inputFilename.substr(dirbase.length) },
+      undefined,
+      4
+    )
+  );
+}
 
-console.log(`[${fs.existsSync(dirsrc)}]: ${dirsrc}`);
-console.log(`[${fs.existsSync(dirdst)}]: ${dirdst}`);
+const dirbase = path.join(__dirname, "../../../../download/node_modules/");
+const dirsrc = path.join(dirbase, "react-native-tscodegen-types/react-native");
+const dirdst = path.join(dirbase, "@types/react-native");
+const dirout = path.join(__dirname, "../output");
+
+const entrysrc = path.join(dirsrc, "index.d.ts");
+const entrydst = path.join(dirdst, "index.d.ts");
+
+if (!fs.existsSync(dirout)) {
+  fs.mkdirSync(dirout, { recursive: true });
+}
+processEntry(entrysrc, path.join(dirout, "source_flow2dts.json"));
+processEntry(entrydst, path.join(dirout, "target_dt.json"));
